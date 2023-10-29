45c8183c1da6a9fc39344b4788e12a482af47ef5
sgmq0 <fengray@seas.upenn.edu>
2023-10-29 00:49:17 -0400

    Walls

--- gamejam2023/Assets/Scripts/Interaction.cs
+++ gamejam2023/Assets/Scripts/Interaction.cs
@@ -9,22 +9,27 @@ public class Interaction : MonoBehaviour
     [SerializeField] private GameObject noteBg;
     [SerializeField] private GameObject noteImage;
 
+    private Transform player;
+    private Transform itemPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player").transform;
+        itemPosition = this.gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    void OnMouseDown() {
-        if (id == "note") {
-            noteImage.SetActive(true);
-            noteBg.SetActive(true);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            
+            float distance = Vector3.Distance(player.position, itemPosition.position);
+            if (distance <= 14) {
+                noteImage.SetActive(true);
+                noteBg.SetActive(true);
+            }
         }
     }
 }
