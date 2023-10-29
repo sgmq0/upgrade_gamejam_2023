4a9eb0403a3ea4d46db0fcdca7866ce55c55e35b
bcaguayo <43423649+bcaguayo@users.noreply.github.com>
2023-10-29 03:01:15 -0400

    Light Initial

--- gamejam2023/Assets/AutoDestroy.cs
+++ gamejam2023/Assets/AutoDestroy.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class AutoDestroy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Sent when an incoming collider makes contact with this object's
+    /// collider (2D physics only).
+    /// </summary>
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    void OnCollisionEnter2D(Collision2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("You Died!");
+        if (other.gameObject.tag == "Enemy") Destroy(gameObject);
     }
 }
