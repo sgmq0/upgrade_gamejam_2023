4e7c738accaf840158e79ce288a81d20751a0d5e
bcaguayo <43423649+bcaguayo@users.noreply.github.com>
2023-10-29 09:53:52 -0400

    Level 1 Death

--- gamejam2023/Assets/Scripts/AutoDestroy.cs
+++ gamejam2023/Assets/Scripts/AutoDestroy.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class AutoDestroy : MonoBehaviour
 {
+    public GameObject deathScreen;
+
+    void Start()
+    {
+        deathScreen.SetActive(false);
+    }
+
     /// <summary>
     /// Sent when an incoming collider makes contact with this object's
     /// collider (2D physics only).
@@ -12,8 +19,11 @@ public class AutoDestroy : MonoBehaviour
     /// <param name="other">The Collision2D data associated with this collision.</param>
     void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("You Died!");
-        SceneManager.LoadScene(3);
-        if (other.gameObject.tag == "Enemy") Destroy(gameObject);
+        if (other.gameObject.tag == "Enemy") {
+            Debug.Log("You Died!");
+            SceneManager.LoadScene(3);
+            Destroy(gameObject);
+            deathScreen.SetActive(true);
+        }
     }
 }
